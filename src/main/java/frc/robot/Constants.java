a9ca3c68d9997c479b081036b44c4edfe54d54d2
Rewand727 <atran255@gmail.com>
2025-01-04 21:25:32 -0700

    Revert "test AndrewT branch"
    This reverts commit 49f2a196632710be3699c6b9961fac4920d038ad.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,7 +13,7 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-  //test 2
+  //test
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
