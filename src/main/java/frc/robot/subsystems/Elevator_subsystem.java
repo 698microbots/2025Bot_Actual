b53d2f82d24b59f7e42b20687aecc5dd1b190f4f
Krishiv Choubey <writetokrishivchoubey@gmail.com>
2025-01-08 16:23:44 -0700

    fixed elevator file name

--- src/main/java/frc/robot/subsystems/Elevator_subsystem.java
+++ src/main/java/frc/robot/subsystems/Elevator_subsystem.java
@@ -9,11 +9,11 @@ import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
-public class elevator_subsytem extends SubsystemBase {
+public class Elevator_subsystem extends SubsystemBase {
   public TalonFX motor1 = new TalonFX(Constants.elevator_motor_1);
   public TalonFX motor2 = new TalonFX(Constants.elevator_motor_2);
   /** Creates a new slevator. */
-  public elevator_subsytem() {}
+  public Elevator_subsystem() {}
 
   public void setspeed(double speed) {
     motor1.set(speed);
