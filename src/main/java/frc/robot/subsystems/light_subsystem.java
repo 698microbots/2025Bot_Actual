674466e4fa9f4fea0eecdd6849f4313e78603fb9
Krishiv Choubey <writetokrishivchoubey@gmail.com>
2025-01-09 16:19:02 -0700

    added phoenix 5

---
+++ src/main/java/frc/robot/subsystems/light_subsystem.java
@@ -0,0 +1,20 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+
+public class light_subsystem extends SubsystemBase {
+  /** Creates a new light_subsystem. */
+  public light_subsystem() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
