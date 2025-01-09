70ac193942f1c86c6bcd305de2280c376aa3a158
Krishiv Choubey <writetokrishivchoubey@gmail.com>
2025-01-09 16:35:10 -0700

    added light subsystem

--- src/main/java/frc/robot/subsystems/light_subsystem.java
+++ src/main/java/frc/robot/subsystems/light_subsystem.java
@@ -6,11 +6,19 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import com.ctre.phoenix.led.CANdleConfiguration;
+import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.CANdle.LEDStripType;
 
 public class light_subsystem extends SubsystemBase {
   /** Creates a new light_subsystem. */
   public light_subsystem() {
+    CANdle candle = new CANdle(0); // creates a new CANdle with ID 0
 
+    CANdleConfiguration config = new CANdleConfiguration();
+    config.stripType = LEDStripType.RGB; // set the strip type to RGB
+    config.brightnessScalar = 0.5; // dim the LEDs to half brightness
+    candle.configAllSettings(config);
   }
 
   @Override
