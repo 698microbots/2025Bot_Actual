844ef73f459be572e36c8720a71bc9a03952f863
Krishiv Choubey <writetokrishivchoubey@gmail.com>
2025-01-22 16:03:56 -0700

    mainly finished with elevator subssytem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,14 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.l1_lift_command;
+import frc.robot.subsystems.Dropper;
+import frc.robot.subsystems.Elevator_subsystem;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.LimeLightSubsystem;
+import frc.robot.subsystems.light_subsystem;
+import frc.robot.subsystems.sensor_subsystem;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -23,11 +30,17 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController m_driverController = new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final CommandXboxController joystick_1 = new CommandXboxController(Constants.joystick_1);
+  private final CommandXboxController joystick_2 = new CommandXboxController(Constants.joystick_2);
+  public Dropper dropper = new Dropper();
+  public Elevator_subsystem elevator = new Elevator_subsystem();
+  public light_subsystem light = new light_subsystem();
+  public sensor_subsystem sensor = new sensor_subsystem();
+  public LimeLightSubsystem limelight = new LimeLightSubsystem();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
+  public RobotContainer(Dropper dropper, Elevator_subsystem elevator, light_subsystem light, sensor_subsystem sensor, LimeLightSubsystem limelight) {
     // Configure the trigger bindings
     configureBindings();
   }
@@ -49,6 +62,7 @@ public class RobotContainer {
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    joystick_1.b().whileTrue(new l1_lift_command(elevator, dropper,l1_lift_command.l2));
   }
 
   /**
