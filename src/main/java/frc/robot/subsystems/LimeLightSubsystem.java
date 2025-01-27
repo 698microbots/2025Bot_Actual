0ee84e31b96a84990e6ace5ed2d5a2c14ac9727c
Rewand727 <atran255@gmail.com>
2025-01-27 14:59:29 -0700

    merge

--- src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimeLightSubsystem.java
@@ -4,6 +4,10 @@
 
 package frc.robot.subsystems;
 
+import java.util.ArrayList;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -11,10 +15,24 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class LimeLightSubsystem extends SubsystemBase {
   /** Creates a new LimeLightSubsystem. */
   NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
-  
+  NetworkTable limelight2 = NetworkTableInstance.getDefault().getTable("limelight2");
+
   public LimeLightSubsystem() {
 
   }
+
+  public Pose3d getPose3dCam1(){
+    Double[] poselist = limelight.getEntry("botpose").getDoubleArray(new Double[6]);
+    return new Pose3d(poselist[0], poselist[1], poselist[2], new Rotation3d(poselist[3], poselist[4], poselist[5]));
+  }
+
+  public Pose3d getPose3dCam2(){
+    Double[] poselist = limelight2.getEntry("botpose").getDoubleArray(new Double[6]);
+    return new Pose3d(poselist[0], poselist[1], poselist[2], new Rotation3d(poselist[3], poselist[4], poselist[5]));
+  }
+
+  
+
 
   @Override
   public void periodic() {
