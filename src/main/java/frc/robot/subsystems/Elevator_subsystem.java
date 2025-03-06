0bf6683c1d63f14524354f54b106e1607ba22bed
mskirthik <46059837+mskirthik@users.noreply.github.com>
2025-03-06 15:04:16 -0700

    updated the subsystem

--- src/main/java/frc/robot/subsystems/Elevator_subsystem.java
+++ src/main/java/frc/robot/subsystems/Elevator_subsystem.java
@@ -10,6 +10,7 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DutyCycleEncoder;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -17,56 +18,62 @@ public class Elevator_subsystem extends SubsystemBase {
   private TalonFX motor1 = new TalonFX(Constants.elevator_motor_1);
   private TalonFX motor2 = new TalonFX(Constants.elevator_motor_2);
   // private DutyCycleEncoder revEncoder = new DutyCycleEncoder(Constants.boreEncoderId);
-  private DutyCycleEncoder revEncoder = new DutyCycleEncoder(Constants.boreEncoderId, 1000, 0);
-
-  DigitalInput toplimitSwitch = new DigitalInput(2);
-  DigitalInput bottomlimitSwitch = new DigitalInput(2);
-  
+  private Encoder revEncoder = new Encoder(0, 1);
+  private DigitalInput limitSwitch = new DigitalInput(2);
   /** Creates a new slevator. */
   
   public Elevator_subsystem() {
-    motor1.setNeutralMode(NeutralModeValue.Coast);
-    motor2.setNeutralMode(NeutralModeValue.Coast);
+    // motor1.setNeutralMode(NeutralModeValue.Coast);
+    // motor2.setNeutralMode(NeutralModeValue.Coast);
+
+    motor1.setNeutralMode(NeutralModeValue.Brake);
+    motor2.setNeutralMode(NeutralModeValue.Brake);
+  }
+
+
+
+  public void setspeed(double speed) {
+    //Max Encoder: 8.077
+    if (getPressed()){
+      revEncoder.reset();
+    }
+    
+    if (getPosition() < .7 && speed < 0){
+      speed = 0;
+    } else if (getPosition() > 7.7 && speed > 0){
+      speed = 0;
+    }
+
+    motor1.set(-speed); //RIGHT NOW POSITIE VALUES FED INTO THE PARAMETER GOES UP
+    motor2.set(-speed); // without direction changes, pushing up on the joystick goes down   
   }
 
   // public void setspeed(double speed) {
-  //   //values of 5 and 1000 are made up for now, use the actual encoder values you get from testing for this
-  //   if (getPosition() < 5 && speed < 0){
-  //     speed = 0;
-  //   } else if (getPosition() > 1000 && speed > 0){
-  //     speed = 0;
+  //   if (toplimitSwitch.get()) {
+  //     motor1.set(0);
+  //     motor2.set(0);
+  //   } else {
+  //     motor1.set(speed);
+  //     motor2.set(speed);
   //   }
-
-  //   motor1.set(-speed);
-  //   motor2.set(-speed); // without direction changes, pushing up on the joystick goes down   
-  // }
-
-  public void setspeed(double speed) {
-    if (toplimitSwitch.get()) {
-      motor1.set(0);
-      motor2.set(0);
-    } else {
-      motor1.set(speed);
-      motor2.set(speed);
-    }
-    if (bottomlimitSwitch.get()) {
-      motor1.set(0);
-      motor2.set(0);
-    } else {
-      motor1.set(speed);
-      motor2.set(speed);
-    }
-  }
+  //   if (bottomlimitSwitch.get()) {
+  //     motor1.set(0);
+  //     motor2.set(0);
+  //   } else {
+  //     motor1.set(speed);
+  //     motor2.set(speed);
+  //   }
  
   public double getPosition() {
-    return revEncoder.get();
+    return revEncoder.get() * -.001;
+  }
+
+  public boolean getPressed(){
+    return limitSwitch.get();
   }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
-
-
-
 }
